3300f3e2ec8e71b3e6ce1ca310b29d4a2c4a45c4
MrX-zeta <brianluisruizperez@gmail.com>
2025-10-20 20:32:32 -0600

    implementacion de routing

--- my-first-proyect/src/app/app-routing-module.ts
+++ my-first-proyect/src/app/app-routing-module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Player } from './player/player';
+import { AudioController } from './audio-controller/audio-controller';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:Player,
+    title: 'Player Music'
+  },
+  {
+    path:'controller',
+    component: AudioController
+  },
+  {
+    path:'view',
+    loadChildren: () => import('./test/test-module').then(m => m.TestModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
